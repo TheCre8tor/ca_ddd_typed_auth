1863dc734a099b1fd9293a0da41fb6b558133f66
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-04-07 11:45:25 +0100

    Added abstract base controller

---
+++ src/shared/infrastructure/http/models/base_controller.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from "express";
+
+export abstract class BaseController {
+  protected abstract executeImpl(
+    req: Request,
+    res: Response
+  ): Promise<void | any>;
+
+  public fail(res: Response, error: Error | string) {
+    console.log(error);
+
+    return res.status(500).json({
+      message: error.toString(),
+    });
+  }
+
+  public async execute(req: Request, res: Response): Promise<void> {
+    try {
+      await this.executeImpl(req, res);
+    } catch (err: any) {
+      console.log(`[BaseController]: Uncaught controller error`);
+      console.log(err);
+      this.fail(res, "An unexpected error occured");
+    }
+  }
+
+  public static jsonResponse(res: Response, code: number, message: string) {
+    return res.status(code).json({ message });
+  }
+
+  public ok<T>(res: Response, dto?: T) {
+    if (!!dto) {
+      res.type("application/json");
+      return res.status(200).json(dto);
+    }
+
+    return res.sendStatus(200);
+  }
+
+  public created(res: Response) {
+    return res.sendStatus(201);
+  }
+
+  // Error Response -->
+
+  public clientError(res: Response, message?: string) {
+    return BaseController.jsonResponse(
+      res,
+      400,
+      message ? message : "Unauthorized"
+    );
+  }
+
+  public unauthorized(res: Response, message?: string) {
+    return BaseController.jsonResponse(
+      res,
+      401,
+      message ? message : "Unauthorized"
+    );
+  }
+
+  public paymentRequired(res: Response, message?: string) {
+    return BaseController.jsonResponse(
+      res,
+      402,
+      message ? message : "Payment Required"
+    );
+  }
+
+  public forbidden(res: Response, message?: string) {
+    return BaseController.jsonResponse(
+      res,
+      403,
+      message ? message : "Forbidden"
+    );
+  }
+}
