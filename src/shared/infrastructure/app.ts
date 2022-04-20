6e59a0b61e4c22445fcdf0541d09aaea8ac4b084
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-04-20 21:14:45 +0100

    Little modification

--- src/shared/infrastructure/app.ts
+++ src/shared/infrastructure/app.ts
@@ -1,10 +1,19 @@
 import "dotenv/config";
 import express from "express";
 import log from "../utils/logger";
+import cors from "cors";
 
 import { v1Router } from "./http/api/v1";
+import { isProduction } from "../../config";
 
 const app = express();
+
+const options = {
+  origin: isProduction ? "http://localhost:3000" : "*",
+};
+
+app.use(cors());
+app.options(options.origin, cors);
 
 app.use(express.json({}));
 
