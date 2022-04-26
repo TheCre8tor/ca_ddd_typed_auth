228a92d6d0f7587db4496e8627519aaa88537feb
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-04-26 11:38:33 +0100

    Initial commit on Comments WatchList value objects

--- src/modules/forum/domain/entities/comment/comment.ts
+++ src/modules/forum/domain/entities/comment/comment.ts
@@ -1,7 +1,9 @@
 import { CommentText } from '../../value_objects/comment/comment_text';
 import { MemberId } from '../member/member_id';
+import { PostId } from '../post/post_id';
 
 export interface CommentProps {
     memberId: MemberId;
+    postId: PostId;
     text: CommentText;
 }
