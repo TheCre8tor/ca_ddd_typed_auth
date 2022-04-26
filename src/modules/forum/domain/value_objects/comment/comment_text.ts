4fabf4080470788f6ffb21a47c837e60e1e4d844
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-04-26 11:22:43 +0100

    Completed CommentText value object

---
+++ src/modules/forum/domain/value_objects/comment/comment_text.ts
@@ -0,0 +1,41 @@
+import { Guard } from '../../../../../shared/core/guard';
+import { Result } from '../../../../../shared/core/result';
+import { ValueObject } from '../../../../../shared/domain/valueObjects/value_object';
+
+interface CommentTextProps {
+    value: string;
+}
+
+export class CommentText extends ValueObject<CommentTextProps> {
+    public static minLength: number = 2;
+    public static maxLength: number = 10000;
+
+    get value(): string {
+        return this.props.value;
+    }
+
+    private constructor(props: CommentTextProps) {
+        super(props);
+    }
+
+    public static create(props: CommentTextProps): Result<CommentText> {
+        const guardResult = Guard.againstNullOrUndefined(props.value, 'commentText');
+
+        if (!guardResult.succeeded) {
+            return Result.fail<CommentText>(guardResult.message!);
+        }
+
+        const minGuardResult = Guard.againstAtLeast(this.minLength, props.value);
+        const maxGuardResult = Guard.againstAtMost(this.maxLength, props.value);
+
+        if (!minGuardResult.succeeded) {
+            return Result.fail<CommentText>(minGuardResult.message!);
+        }
+
+        if (!maxGuardResult.succeeded) {
+            return Result.fail<CommentText>(maxGuardResult.message!);
+        }
+
+        return Result.ok<CommentText>(new CommentText(props));
+    }
+}
