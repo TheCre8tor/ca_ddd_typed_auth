7f86dcaf81fd82d30ac17382763042bcefe566ea
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-04-26 11:27:18 +0100

    Initial commit for PostID entity

--- src/modules/forum/domain/entities/comment/comment.ts
+++ src/modules/forum/domain/entities/comment/comment.ts
@@ -1,6 +1,7 @@
+import { CommentText } from '../../value_objects/comment/comment_text';
 import { MemberId } from '../member/member_id';
 
 export interface CommentProps {
     memberId: MemberId;
-    // text:
+    text: CommentText;
 }
